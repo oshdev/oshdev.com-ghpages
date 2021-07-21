54497ab13e7840ad758b5798d7835902546e9dc4
osh <8842282+oshdev@users.noreply.github.com>
2021-07-21 23:45:33 +0100

    feat: more styles and analytics

--- static/global.js
+++ static/global.js
@@ -1,9 +1,73 @@
-(function () {
+function bakeCookie(name, value, options = {}) {
+  const defaultOptions = {
+    Path: '/',
+    'Max-age': 60 * 60 * 24 * 365,
+    Secure: undefined,
+    SameSite: 'strict',
+  }
+  document.cookie = Object.entries({...defaultOptions, ...options}).reduce((a, [k, v]) => {
+    a += `;${k}`
+    if (typeof v !== 'undefined') a += `=${v}`
+    return a
+  }, `${name}=${encodeURIComponent(value)}`)
+  console.log(document.cookie)
+}
+
+function burnCookie(name) {
+  bakeCookie(name, '', {'Max-age': 0})
+}
+
+function eatCookie(name) {
+  const cookie = document.cookie.split(';').find(c => c.trim().startsWith(name));
+  return cookie && decodeURIComponent(cookie.split('=', 2)[1].trim())
+}
+
+function enableAnalytics() {
+  bakeCookie('analyticsEnabled', '1')
+
+  if (document.getElementById('gTagSrc')) return
+
+  const gTagId = 'UA-60548914-1'
+
+  const gTagSrc = document.createElement('script')
+  gTagSrc.setAttribute('id', 'gTagSrc')
+  gTagSrc.setAttribute('async', 'async')
+  gTagSrc.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${gTagId}`)
+  document.head.appendChild(gTagSrc)
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', 'UA-60548914-1');
+}
+
+function disableAnalytics() {
+  burnCookie('analyticsEnabled')
+
+  const gTagSrc = document.getElementById('gTagSrc')
+  if (gTagSrc) gTagSrc.remove()
+
+  window.dataLayer = undefined
+}
+
+function setTheme(theme) {
+  document.documentElement.setAttribute('theme', theme)
+  bakeCookie('theme', theme)
+}
+
+
+;(function () {
+  if (eatCookie('analyticsEnabled')) enableAnalytics()
+}())
+
+;(function () {
   const checkTheme = maybeTheme => ['light', 'dark'].includes(maybeTheme) ? maybeTheme : undefined
 
-  const userCookieThemeString = document.cookie.split(';').find(c => c.startsWith('theme='))
-  const userCookieTheme = userCookieThemeString && userCookieThemeString.split('=')[1]
-  const cookieTheme = checkTheme(userCookieTheme)
+  const cookieTheme = checkTheme(eatCookie('theme'))
   const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 
   const theme = cookieTheme ? cookieTheme : systemTheme ? systemTheme : 'dark'
