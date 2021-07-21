e03689e10042116e50e9ce0fc2307c8c352f65d0
osh <8842282+oshdev@users.noreply.github.com>
2021-07-22 00:01:55 +0100

    refactor: separate cookie and theme from other js

--- static/global.js
+++ static/global.js
@@ -1,27 +1,3 @@
-function bakeCookie(name, value, options = {}) {
-  const defaultOptions = {
-    Path: '/',
-    'Max-age': 60 * 60 * 24 * 365,
-    Secure: undefined,
-    SameSite: 'strict',
-  }
-  document.cookie = Object.entries({...defaultOptions, ...options}).reduce((a, [k, v]) => {
-    a += `;${k}`
-    if (typeof v !== 'undefined') a += `=${v}`
-    return a
-  }, `${name}=${encodeURIComponent(value)}`)
-  console.log(document.cookie)
-}
-
-function burnCookie(name) {
-  bakeCookie(name, '', {'Max-age': 0})
-}
-
-function eatCookie(name) {
-  const cookie = document.cookie.split(';').find(c => c.trim().startsWith(name));
-  return cookie && decodeURIComponent(cookie.split('=', 2)[1].trim())
-}
-
 function enableAnalytics() {
   bakeCookie('analyticsEnabled', '1')
 
@@ -54,22 +30,7 @@ function disableAnalytics() {
   window.dataLayer = undefined
 }
 
-function setTheme(theme) {
-  document.documentElement.setAttribute('theme', theme)
-  bakeCookie('theme', theme)
-}
-
 
 ;(function () {
   if (eatCookie('analyticsEnabled')) enableAnalytics()
-}())
-
-;(function () {
-  const checkTheme = maybeTheme => ['light', 'dark'].includes(maybeTheme) ? maybeTheme : undefined
-
-  const cookieTheme = checkTheme(eatCookie('theme'))
-  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-
-  const theme = cookieTheme ? cookieTheme : systemTheme ? systemTheme : 'dark'
-  document.documentElement.setAttribute('theme', theme)
 }())
