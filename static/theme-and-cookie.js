91e1b500260c66cb731e26bb4abcae28ef975f78
osh <8842282+oshdev@users.noreply.github.com>
2021-08-15 19:14:08 +0100

    test: it selects theme based on cookie value

--- static/theme-and-cookie.js
+++ static/theme-and-cookie.js
@@ -5,7 +5,7 @@ function bakeCookie(name, value, options = {}) {
     Secure: undefined,
     SameSite: 'strict',
   }
-  document.cookie = Object.entries({...defaultOptions, ...options}).reduce((a, [k, v]) => {
+  document.cookie = Object.entries({ ...defaultOptions, ...options }).reduce((a, [k, v]) => {
     a += `;${k}`
     if (typeof v !== 'undefined') a += `=${v}`
     return a
@@ -13,25 +13,26 @@ function bakeCookie(name, value, options = {}) {
 }
 
 function burnCookie(name) {
-  bakeCookie(name, '', {'Max-age': 0})
+  bakeCookie(name, '', { 'Max-age': 0 })
 }
 
 function eatCookie(name) {
-  const cookie = document.cookie.split(';').find(c => c.trim().startsWith(name));
+  const cookie = document.cookie.split(';').find(c => c.trim().startsWith(name))
   return cookie && decodeURIComponent(cookie.split('=', 2)[1].trim())
 }
 
-function setTheme(theme) {
-  document.documentElement.setAttribute('theme', theme)
-  bakeCookie('theme', theme)
-}
-
-
-;(function () {
+function updateTheme() {
   const checkTheme = maybeTheme => ['light', 'dark'].includes(maybeTheme) ? maybeTheme : undefined
 
   const cookieTheme = checkTheme(eatCookie('theme'))
   const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   const theme = cookieTheme ? cookieTheme : systemTheme ? systemTheme : 'dark'
   document.documentElement.setAttribute('theme', theme)
-}())
+}
+
+function setTheme(theme) {
+  bakeCookie('theme', theme)
+  updateTheme()
+}
+
+updateTheme()
